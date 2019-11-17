225778a2b3f2c3a2406d372b63aecfa7e675f6c9
alexa <alexa.griffin42@gmail.com>
2019-11-17 17:58:05 -0500

    yee

--- stratus-macros/src/lib.rs
+++ stratus-macros/src/lib.rs
@@ -1,12 +1,20 @@
 extern crate proc_macro;
 use proc_macro::*;
 
-//TODO: make this work with the function name
+extern crate syn;
+
 #[proc_macro_attribute]
-pub fn timed_fn(args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn timed_fn(_args: TokenStream, input: TokenStream) -> TokenStream {
     use std::str::FromStr;
 
+    let fn_name = {
+        let data = input.clone();
+        let tokens = syn::parse_macro_input!(data as syn::ItemFn);
+        tokens.sig.ident.to_string()
+    };
+
     let mut token_str = input.to_string();
-    token_str.insert_str(token_str.find("{").expect("") + 1, "let __internal_stratus_timer = stratus::Timer::default();");
+    token_str.insert_str(token_str.find("{").expect("") + 1, 
+        &format!("let __internal_stratus_timer = stratus::Timer::new_with_label(\"{}\");", fn_name));
     TokenStream::from_str(&token_str).expect("generated invalid tokens")
 }
